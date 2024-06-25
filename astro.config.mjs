bf3cd931c4da7716fa63e990e59dcdfd698805e9
cyrus01337 <cyrus21337@gmail.com>
2024-06-25 04:44:25 +0000

    Reconfigure to use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "/",
     integrations: [tailwind()],
     server: {
         port: 3000,
         host: true,
     },
-    site: "https://cyrus01337.github.io",
+    site: "https://cyrus01337.co.uk",
 });
