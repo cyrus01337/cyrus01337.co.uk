8ef9dcfd926acb5a84327b1aa3791345d7bd7949
cyrus01337 <cyrus21337@gmail.com>
2024-07-26 07:24:51 +0000

    Replace React with Preact

--- astro.config.js
+++ astro.config.js
@@ -1,12 +1,11 @@
 import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), react()],
+    integrations: [tailwind(), mdx(), preact({ compat: true })],
     server: {
         port: 3000,
         host: true,
